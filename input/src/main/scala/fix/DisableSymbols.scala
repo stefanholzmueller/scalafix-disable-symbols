3a077a68c49269535385c2d4993d794896d01854
Stefan <stefanholzmueller@users.noreply.github.com>
2023-11-05 14:32:58 +0100

    better tests

--- input/src/main/scala/fix/DisableSymbols.scala
+++ input/src/main/scala/fix/DisableSymbols.scala
@@ -11,9 +11,14 @@ object DisableSymbols {
   println("forbidden") // assert: DisableSymbols
   println("ok") // scalafix:ok; suppressed
   println() // assert: DisableSymbols
-  Nil.foreach(println) // assert: DisableSymbols
+  Nil.foreach(println) /* assert: DisableSymbols
+              ^^^^^^^
+  use logging instead
+   */
   System.out.println("this is not forbidden")
   Console.println("this is not forbidden")
-  Console.print("forbidden") // assert: DisableSymbols
-  ??? // not forbidden
+  Console.print("forbidden") /* assert: DisableSymbols
+          ^^^^^
+  scala.Console.print is disabled by the DisableSymbols rule
+   */
 }
