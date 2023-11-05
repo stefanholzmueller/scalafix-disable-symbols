d900bca56e0c7bc46ebf371a4dbd6f237cc7e9da
Stefan <stefanholzmueller@users.noreply.github.com>
2023-11-05 16:14:58 +0100

    TODO

--- input/src/main/scala/fix/DisableSymbolsScala2.scala
+++ input/src/main/scala/fix/DisableSymbolsScala2.scala
@@ -2,12 +2,16 @@
 rule = DisableSymbols
 DisableSymbols.symbols = [
   { symbol: "scala.Predef.println", message: "use logging instead" },
-  "scala.Console.print"
+  "scala.Console.print",
+  "java.lang.Throwable.printStackTrace",
+  { symbol: "scala.Predef.???", message: "did you forget something?" }
 ]
  */
 package fix
 
-object DisableSymbols {
+import java.io.PrintWriter
+
+object DisableSymbolsScala2 {
   println("forbidden") // assert: DisableSymbols
   println("ok") // scalafix:ok; suppressed
   println() // assert: DisableSymbols
@@ -21,4 +25,11 @@ object DisableSymbols {
           ^^^^^
   scala.Console.print is disabled by the DisableSymbols rule
    */
+  new RuntimeException("forbidden").printStackTrace() // assert: DisableSymbols
+  new RuntimeException("forbidden").printStackTrace(new PrintWriter("test.txt")) // assert: DisableSymbols
+  // TODO
+//  val x = ??? /* assert: DisableSymbols
+//  ^^^
+//  did you forget something ?
+//  */
 }
